a83ae0e97d10c5b3f542fb4c4e4d1997341828e4
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-05-14 15:17:03 +0400

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,16 +11,16 @@ export const Footer = () => {
               <h4>ჩვენ შესახებ</h4>
               <ul>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">წესები</a>
+                  <a href="/" rel="nofollow noreferrer">წესები</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">კონფიდენციალურობა</a>
+                  <a href="/" rel="nofollow noreferrer">კონფიდენციალურობა</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">კონტაქტი</a>
+                  <a href="/" rel="nofollow noreferrer">კონტაქტი</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">ხშირად დასმული კითხვები</a>
+                  <a href="/" rel="nofollow noreferrer">ხშირად დასმული კითხვები</a>
                 </li>
               </ul>
             </div>
@@ -28,19 +28,19 @@ export const Footer = () => {
               <h4>ნავიგაცია</h4>
               <ul>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">შესვლა</a>
+                  <a href="/" rel="nofollow noreferrer">შესვლა</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">რეგისტრაცია</a>
+                  <a href="/" rel="nofollow noreferrer">რეგისტრაცია</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">თამაშები</a>
+                  <a href="/" rel="nofollow noreferrer">თამაშები</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">ძიება</a>
+                  <a href="/" rel="nofollow noreferrer">ძიება</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">კომპონენტი</a>
+                  <a href="/" rel="nofollow noreferrer">კომპონენტი</a>
                 </li>
               </ul>
             </div>
@@ -48,32 +48,32 @@ export const Footer = () => {
               <h4>ლოგარითმი</h4>
               <ul>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">ალბერტ აინშტაინი</a>
+                  <a href="/" rel="nofollow noreferrer">ალბერტ აინშტაინი</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">ნიკა შამილაძე</a>
+                  <a href="/" rel="nofollow noreferrer">ნიკა შამილაძე</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">მარკ ცუკერბერგი</a>
+                  <a href="/" rel="nofollow noreferrer">მარკ ცუკერბერგი</a>
                 </li>
                 <li>
-                  <a href="#" rel="nofollow noreferrer">ილონ მასკი</a>
+                  <a href="/" rel="nofollow noreferrer">ილონ მასკი</a>
                 </li>
               </ul>
             </div>
             <div className="footer-col">
               <h4>სოციალური ქსელები</h4>
               <div className="social-links">
-                <a href="#" rel="nofollow noreferrer">
+                <a href="/" rel="nofollow noreferrer">
                   <i className="fab fa-facebook-f"></i>
                 </a>
-                <a href="#" rel="nofollow noreferrer">
+                <a href="/" rel="nofollow noreferrer">
                   <i className="fab fa-twitter"></i>
                 </a>
-                <a href="#" rel="nofollow noreferrer">
+                <a href="/" rel="nofollow noreferrer">
                   <i className="fab fa-instagram"></i>
                 </a>
-                <a href="#" rel="nofollow noreferrer">
+                <a href="/" rel="nofollow noreferrer">
                   <i className="fab fa-linkedin-in"></i>
                 </a>
               </div>
